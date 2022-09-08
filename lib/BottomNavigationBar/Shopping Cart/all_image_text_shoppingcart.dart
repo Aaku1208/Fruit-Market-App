606d8c47ba256b771d631cb9a93061bf94520129
Aaku1208 <ava.globaliasoft@gmail.com>
2022-09-08 17:41:27 +0530

    first commit

---
+++ lib/BottomNavigationBar/Shopping Cart/all_image_text_shoppingcart.dart
@@ -0,0 +1,17 @@
+List<Map> ShoppingCartImageText = [
+  {
+    'image': 'https://images.unsplash.com/photo-1594282486552-05b4d80fbb9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FiYmFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60',
+    'subtitle1': 'Cabbage',
+    'subtitle2': 'Rs 40 Saved',
+    'subtitle3': 'Rs 190',
+    'subtitle4': '150 Per / kg',
+  },
+
+  {
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ08lDuITKopMHHWZR0mPzT1dOivQqEm-QWkw&usqp=CAU',
+    'subtitle1': 'Red Onions',
+    'subtitle2': 'Rs 60 Saved',
+    'subtitle3': 'Rs 260',
+    'subtitle4': '200 Per / kg',
+  },
+];
--- lib/BottomNavigationBar/Shopping Cart/shopping_cart.dart
+++ lib/BottomNavigationBar/Shopping Cart/shopping_cart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_market2/BottomNavigationBar/Shopping%20Cart/all_image_text_shoppingcart.dart';
 
 import '../../Colors/color.dart';
 
@@ -12,14 +13,215 @@ class ShoppingPage extends StatefulWidget {
 class _ShoppingPageState extends State<ShoppingPage> {
   @override
   Widget build(BuildContext context) {
+    double sizehight = MediaQuery.of(context).size.height;
+    double sizewidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
         backgroundColor: myColor,
         title: Text("My Cart"),
       ),
-      body: Center(
-        child: Text("Under Production"),
+      body: Column(
+        children: [
+          Container(
+            height: 60,
+            width: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 19),
+                      child: Icon(
+                        Icons.location_on,
+                        size: 20,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text("440001 Nagpur,Maharashtra"),
+                    Icon(Icons.keyboard_arrow_down_rounded, size: 30),
+                    SizedBox(width: 15),
+                    Text(
+                      "Change Address",
+                      style: TextStyle(color: Color(0xff7089F0), fontSize: 12),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          // Vegetables
+          Container(
+            height: 33,
+            width: double.infinity,
+            color: Colors.grey.shade300,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Vegetables",
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+          ),
+          Container(
+            height: 306,
+            width: double.infinity,
+            //color: Colors.grey,
+            child: Column(
+              children: [
+                ListView.separated(
+                  itemCount: ShoppingCartImageText.length,
+                  scrollDirection: Axis.vertical,
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Row(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 15, top: 15, right: 30, bottom: 9),
+                            child: Container(
+                              height: 120,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(17),
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    ShoppingCartImageText[index]['image'],
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              child: Text(""),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            height: 122,
+                            child: Column(
+                              //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    SizedBox(height: 51),
+                                    Text(
+                                        ShoppingCartImageText[index]
+                                            ['subtitle1'],
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w600)),
+                                    SizedBox(width: 10),
+                                    Text(
+                                        ShoppingCartImageText[index]
+                                            ['subtitle2'],
+                                        style: TextStyle(
+                                            color: myColor, fontSize: 11)),
+                                  ],
+                                ),
+                                // SizedBox(height: 0),
+                                Text(ShoppingCartImageText[index]['subtitle3'],
+                                    style: const TextStyle(fontSize: 12,
+                                      decoration: TextDecoration.lineThrough,
+                                    )),
+                                SizedBox(height: 8),
+                                Text(
+                                  ShoppingCartImageText[index]['subtitle4'],
+                                  style: TextStyle(
+                                      fontSize: 11,
+                                      fontWeight: FontWeight.w700)),
+                                SizedBox(height: 10),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 87),
+                                  child: Row(
+                                    children: [
+                                      /// -
+                                      Container(
+                                        height: 25,
+                                        width: 25,
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: const BorderRadius.all(Radius.circular(7)),
+                                          border: Border.all(
+                                            color: Colors.black54,
+                                            width: 1.6,
+                                          ),
+                                        ),
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            Icon(Icons.remove,size: 15),
+                                          ],
+                                        ),
+                                      ),
+                                      SizedBox(width: 10),
+                                      Row(
+                                        children: [
+                                          Text("2")
+                                        ],
+                                      ),
+                                      SizedBox(width: 10),
+                                      /// +
+                                      Container(
+                                        height: 25,
+                                        width: 25,
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: const BorderRadius.all(Radius.circular(7)),
+                                          border: Border.all(
+                                            color: Colors.black54,
+                                            width: 1.6,
+                                          ),
+                                        ),
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            Icon(Icons.add,size: 15),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 9, top: 9),
+                          width: sizewidth * 0.085,
+                          // 0.076
+                          alignment: Alignment.topRight,
+                          height: 100,
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.delete_outline_outlined,
+                                color: Colors.black,
+                                size: 20,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return Divider(
+                      thickness: 1,
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
